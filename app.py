59aab0a6785f3242e3c583ba6268431f3e555c56
Danielle Meer <55391597+TIGERTIDES@users.noreply.github.com>
2024-09-19 22:15:12 +0800

    MAPUA

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Cathy Dela Cruz's News Articles", layout="centered")
 
-dataframed = pd.read_csv("https://raw.githubusercontent.com/ALGOREX-PH/AI_Republic_Miguel_Migs_Cruz_ADMU/main/Dataset/Articles_AI.csv")
+dataframed = pd.read_csv("https://raw.githubusercontent.com/ALGOREX-PH/AI_Republic_Cathy_Dela_Cruz_MAPUA/main/Dataset/Articles_AI.csv")
 
 if 'current_page' not in st.session_state:
     st.session_state.current_page = 'Home'
